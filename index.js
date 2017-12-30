80f44e641036596baa35992babca58f779bcf4cb
Kelsey M. Beard <kay.beard.design@gmail.com>
2017-12-30 07:50:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === "I love you, Grandma.") {
     return "I love you, too."
-  } else if (string === string.isUpperCase()) {
+  } else if (string === string.isUpperCase {
     return "YES INDEED!"
   } else {
     return "I can't hear you!"
